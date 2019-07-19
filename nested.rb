0922f66aac54ba8410c94d8818356888c979e0ae
J K <kim.ju652@gmail.com>
2019-07-19 05:59:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] =
+     language = programmer_hash[:dennis_ritchie][:languages]
+     language <= "Assembly"
 
 end
